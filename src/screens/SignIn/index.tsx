f9934e22a4980a87e7cbb8c22bb9bbff8dc26837
Alberto Lacerda <albertoflfilho@gmail.com>
2024-02-27 18:18:36 -0500

    adding button component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,15 @@
+
 import backgroundImg from '../../assets/background.png';
+import { Button } from '../../components/Button';
 import { Container, Title, Slogan } from './styles';
 
 export function SignIn() {
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
-      <Slogan>Open up App.tsx to start working on your App!</Slogan>
+      <Slogan>The live stream dream!</Slogan>
+
+      <Button title="Sign in with Google" onPress={() => {}} />
     </Container>
   );
 }
