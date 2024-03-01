0d417691275a0702b1f2941f57a6fa1b4b5209cc
Alberto Lacerda <albertoflfilho@gmail.com>
2024-03-01 11:14:46 -0500

    refactoring signIn with google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,69 +1,52 @@
-import { useEffect, useState } from 'react';
-import { Alert } from 'react-native';
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google';
-
-import { Realm, useApp } from '@realm/react';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-
+import { useState } from 'react';
 import { Container, Title, Slogan } from './styles';
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
+import { Realm, useApp } from '@realm/react'
 
-import backgroundImg from '../../assets/background.png';
+import backgroundImg from '../../assets/background.png'
 import { Button } from '../../components/Button';
 
-import { IOS_CLIENT_ID, WEB_CLIENT_ID, ANDROID_CLIENT_ID } from '@env';
+import { WEB_CLIENT_ID, IOS_CLIENT_ID } from '@env'
+import { Alert } from 'react-native';
 
-WebBrowser.maybeCompleteAuthSession();
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID
+})
 
 export function SignIn() {
-  const [ isAuthenticating, setIsAuthenticating ] = useState(false);
+  const [isAutenticating, setIsAuthenticanting] = useState(false)
+  const app = useApp()
 
-  const app = useApp();
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticanting(true)
 
-  const [_, response, googleSignIng] = Google.useAuthRequest({
-    androidClientId: ANDROID_CLIENT_ID,
-    iosClientId: IOS_CLIENT_ID,
-    scopes: ['profile', 'email']
-  });
+      const { idToken } = await GoogleSignin.signIn()
+      
+      if(idToken) {
+        const credentials = Realm.Credentials.jwt(idToken)
 
-  function handleGoogleSignIn(){
-    setIsAuthenticating(true);
-
-    googleSignIng().then(response => {
-      if(response?.type !== 'success') {
-        setIsAuthenticating(false);
-      }
-    })
-  }
-
-  useEffect(() => {
-    if(response?.type === 'success') {
-      if(response.authentication?.idToken) {
-        const credentials = Realm.Credentials.jwt(response.authentication.idToken);
-
-        app.logIn(credentials).catch((error) => {
-          console.log(error);
-          Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta google.')
-          setIsAuthenticating(false);
-        })
+        await app.logIn(credentials)
       } else {
-        Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta google.')
-        setIsAuthenticating(false);
+        Alert.alert('SignIn', "Unable to connect to your Google account.")
+        setIsAuthenticanting(false)  
       }
+
+    } catch (error) {
+      console.log(error)
+      Alert.alert('SignIn', "Unable to connect to your Google account.")
+      setIsAuthenticanting(false)
     }
-  },[response])
+  }
 
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Car use management</Slogan>
 
-      <Button 
-        title="Sign in with Google" 
-        onPress={handleGoogleSignIn} 
-        isLoading={isAuthenticating}
-      />
+      <Button title='Entrar com Google' onPress={handleGoogleSignIn} isLoading={isAutenticating} />
     </Container>
   );
 }
-
