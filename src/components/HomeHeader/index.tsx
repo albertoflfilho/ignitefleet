a1c5fbd401ecf508caf49e6a568ff73cefc8e973
Alberto Lacerda <albertoflfilho@gmail.com>
2024-03-06 10:48:52 -0500

    adding realm-image - fetching data with realm - logout functionality

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,17 +1,28 @@
 import { TouchableOpacity } from 'react-native';
-import { Power } from 'phosphor-react-native'; 
-import { Container, Greeting, Message, Name } from './styles';
+import { Power } from 'phosphor-react-native';
+import { useUser, useApp } from '@realm/react';
+import { Container, Greeting, Message, Name, Picture } from './styles';
 import theme from '../../theme';
 
 export function HomeHeader() {
+  const user = useUser();
+  const app = useApp();
+
+  function handleLogout() {
+    app.currentUser?.logOut();
+  }
+
   return (
     <Container>
+        <Picture source={{ uri: user?.profile.pictureurl }}
+          placeholder="L184i9kCbIof00ayjZay~qj[ayj@"
+        />
         <Greeting>
-            <Message>Hello</Message>
-            <Name>Alberto Lacerda</Name>
+            <Message>Hello,</Message>
+            <Name>{user?.profile.name}</Name>
         </Greeting>
 
-        <TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7} onPress={handleLogout}>
             <Power size={32} color={theme.COLORS.GRAY_400} />
         </TouchableOpacity>
     </Container>
